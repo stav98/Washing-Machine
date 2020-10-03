2496ed8de2fc48160aa77ce31f0a49b5f0327b1f
Stavros Fotoglou <38467107+stav98@users.noreply.github.com>
2020-10-03 23:45:39 +0300

    Update functions.h

--- src/functions.h
+++ src/functions.h
@@ -1,3 +1,19 @@
+/*Washing Machine Controller Program for ESP32
+Copyright (C) 2020  Stavros S. Fotoglou
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.*/
+
 //Τα χρονικά σημεία μεταξύ των ενεργειών
 unsigned short points[30]; //Χρόνοι σε sec
 
