bfb80aa07c6166e5249a97e89769d936d1306df3
Stavros Fotoglou <38467107+stav98@users.noreply.github.com>
2020-10-03 23:47:21 +0300

    Update motor.h

--- src/motor.h
+++ src/motor.h
@@ -1,3 +1,19 @@
+/*Washing Machine Controller Program for ESP32
+Copyright (C) 2020  Stavros S. Fotoglou
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.*/
+
 //Η τροχαλία έχει σχέση 1:20, δηλαδή για 800 στροφές στύψιμο το μοτέρ γυρίζει με 16000.
 //Το συγκεκριμένο μοντέλο είναι για max 600 RPM δηλαδή 12000 στο μοτέρ.
 //Για πλύσιμο έχω 60 RPM δηλ. 1200.
